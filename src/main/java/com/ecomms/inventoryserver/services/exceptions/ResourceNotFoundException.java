20c05b726bdda6849476e99b62d10f9388d72bd0
gmtommasini <gmtommasini@gmail.com>
2023-06-28 10:51:21 -0400

    ResourceNotFoundException message

--- src/main/java/com/ecomms/inventoryserver/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/ecomms/inventoryserver/services/exceptions/ResourceNotFoundException.java
@@ -3,8 +3,8 @@ package com.ecomms.inventoryserver.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public ResourceNotFoundException(String msg) {
-        super(msg);
+    public ResourceNotFoundException(String productID) {
+        super(String.format("Product with id %s not found in the inventory.", productID));
     }
 
 }
