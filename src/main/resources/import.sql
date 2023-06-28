ad3e6e8b09720fd9350f3f2cd1e644c5d8fc445b
gmtommasini <gmtommasini@gmail.com>
2023-06-28 10:50:30 -0400

    db seed for local test

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,5 @@
-CREATE TABLE tb_inventory (
-  product_id VARCHAR(255),
-  quantity INT
-);
-
 INSERT INTO tb_inventory (product_id, quantity) VALUES ('id_1', 3);
 INSERT INTO tb_inventory (product_id, quantity) VALUES ('id_2', 30);
 INSERT INTO tb_inventory (product_id, quantity) VALUES ('id_3', 300);
 INSERT INTO tb_inventory (product_id, quantity) VALUES ('id_4', 25);
-
+--
