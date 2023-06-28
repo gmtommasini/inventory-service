dfe063525e109ff66df7e423a3048644379e8c4a
gmtommasini <gmtommasini@gmail.com>
2023-06-28 13:26:17 -0400

    controller with wildcard

--- src/main/java/com/ecomms/inventoryserver/controllers/InventoryController.java
+++ src/main/java/com/ecomms/inventoryserver/controllers/InventoryController.java
@@ -3,7 +3,9 @@ package com.ecomms.inventoryserver.controllers;
 import com.ecomms.inventoryserver.dto.InventoryItemDTO;
 import com.ecomms.inventoryserver.services.InventoryService;
 
+import com.ecomms.inventoryserver.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +19,6 @@ public class InventoryController {
     @Autowired
     private InventoryService service;
 
-//    private final InventoryService service;
-//    public InventoryController(InventoryService service) {this.service = service;}
-
     /**
      * Health check. Does nothing
      * @return 200
@@ -31,12 +30,19 @@ public class InventoryController {
 
     /**
      * Fetches a complete Inventory Item
-     * @param productId
+     * @param productId product unique identifier
      * @return json representing an Inventory Item
      */
     @GetMapping("/{productId}")
-    public ResponseEntity<InventoryItemDTO> getInventoryItem(@PathVariable("productId") String productId){
-        InventoryItemDTO item =  service.getInventoryItem(productId);
-        return ResponseEntity.ok().body(item);
+    public ResponseEntity<?> getInventoryItem(@PathVariable("productId") String productId){
+        try {
+            InventoryItemDTO item =  service.getInventoryItem(productId);
+            return ResponseEntity.ok().body(item);
+        }catch (ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }catch (Exception e){
+            // TODO: Create a log here: System.out.println("INTERNAL ERROR: " + e.getMessage());
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
--- src/test/java/com/ecomms/inventoryserver/controllers/InventoryControllerTest.java
+++ src/test/java/com/ecomms/inventoryserver/controllers/InventoryControllerTest.java
@@ -3,12 +3,14 @@ package com.ecomms.inventoryserver.controllers;
 
 import com.ecomms.inventoryserver.dto.InventoryItemDTO;
 import com.ecomms.inventoryserver.services.InventoryService;
+import com.ecomms.inventoryserver.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -17,7 +19,12 @@ import static org.mockito.Mockito.*;
 
 
 @ExtendWith(SpringExtension.class)
-public class InventoryControllerTest {
+class InventoryControllerTest {
+    String validProductId ;
+    String invalidProductId ;
+    Integer validQuantity;
+    InventoryItemDTO expectedInventoryItem;
+
 
     @Mock
     private InventoryService inventoryService;
@@ -28,23 +35,35 @@ public class InventoryControllerTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        validProductId = "123";
+        invalidProductId = "XXXX";
+        validQuantity = 10;
+        expectedInventoryItem = new InventoryItemDTO(validProductId, validQuantity);
     }
 
     @Test
-    public void testGetInventoryItem() throws Exception {
+    void getShouldReturnItemWhenIdValid() throws Exception {
         // Arrange
-        String productId = "123";
-        Integer quantity = 10;
-        InventoryItemDTO expectedInventoryItem = new InventoryItemDTO(productId, quantity);
-        when(inventoryService.getInventoryItem(productId)).thenReturn(expectedInventoryItem);
+        when(inventoryService.getInventoryItem(validProductId)).thenReturn(expectedInventoryItem);
 
         //Act
-        ResponseEntity<InventoryItemDTO> responseItem = inventoryController.getInventoryItem(productId);
+        ResponseEntity<InventoryItemDTO> responseItem = (ResponseEntity<InventoryItemDTO>) inventoryController.getInventoryItem(validProductId);
 
         // Assert
-        assertEquals(productId, responseItem.getBody().getProductId());
-        assertEquals(quantity, responseItem.getBody().getQuantity());
+        assertEquals(validProductId, responseItem.getBody().getProductId());
+        assertEquals(validQuantity, responseItem.getBody().getQuantity());
 
-        verify(inventoryService, times(1)).getInventoryItem(productId);
+        verify(inventoryService, times(1)).getInventoryItem(validProductId);
+    }
+
+    @Test
+    void getShouldReturnNotFoundWhenIdInvalid() {
+        when(inventoryService.getInventoryItem(invalidProductId)).thenThrow(new ResourceNotFoundException(invalidProductId));
+
+        ResponseEntity<?> response = inventoryController.getInventoryItem(invalidProductId);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(String.format("Product with id %s not found in the inventory.", invalidProductId), response.getBody());
+        verify(inventoryService, times(1)).getInventoryItem(invalidProductId);
     }
 }
